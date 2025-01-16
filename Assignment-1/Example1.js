869f7f092df5604e003096bf6b84621d5fcb9800
Caleb <cqlebrg@gmail.com>
2025-01-16 15:45:24 -0600

    Finsihed Assignment 1

---
+++ Assignment-1/Example1.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 200);
+  }
+  
+  function draw() {
+    background("lime");
+    
+    strokeWeight(2);
+    fill("white");
+    circle(100,100,150);
+    rect(225,25,150,150);
+  }
